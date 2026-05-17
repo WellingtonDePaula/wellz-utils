18f8f9bd90a7c9e930def80ba722da124bb21726
Wellz <owellingtondepaula@gmail.com>
2026-05-17 17:04:55 -0300

    Add initial Unity project files and .gitignore
    Bootstrap Unity project: add core Assets (SampleScene, Testing.cs, InputSystem_Actions, DefaultVolumeProfile, renderer/URP assets and scene templates) and full ProjectSettings (graphics, audio, input, URP, etc.). Update .gitignore to include standard Unity ignores and extra entries for IDEs/plugins. These changes initialize the project structure and default engine/settings needed to open and run the project.

---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using Wellz.Core.Utils;
+
+public class Testing : MonoBehaviour {
+    //private GenericGrid grid;
+    //private void Start() {
+    //    grid = new GenericGrid(10, 10, 1f);
+    //}
+
+    //private void Update() {
+    //    if (Input.GetMouseButtonDown(0)) {
+    //        Vector3 mousePos = UtilsClass.GetMouseWorldPosition();
+
+    //        grid.SetValue(mousePos, 10);
+    //    }
+    //}
+}
